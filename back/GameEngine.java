e0f14399582f496d5716426ff8b9a95b7e4b87df
Mikkel <mikkel.aarby@outlook.dk>
2026-07-16 19:15:33 +0200

    checkpoint

--- back/GameEngine.java
+++ back/GameEngine.java
@@ -1,13 +1,51 @@
 import java.util.ArrayList;
+import java.util.Date;
 
 public class GameEngine {
   ArrayList<Integer> cards = new ArrayList<Integer>();
-  public ArrayList<Integer> makeCards() {
-    for (int i=0; i<9; i++) {
+  String Player0;
+  String Player1;
+
+
+  public void makeCards() {
+    for (int i=0; i<10; i++) {
       int card = (int) Math.floor(Math.random() * 6);
       cards.add(card);
     }
     System.out.println(cards);
-    return cards;
+  }
+
+  public ArrayList<Integer> Draw(int amount) {
+    if (amount < 0) {
+      return new ArrayList<Integer>(-1);
+    }
+    if (cards.size() < amount) {
+      amount = cards.size();
+    }
+
+    ArrayList<Integer> draw = new ArrayList<Integer>();
+    for (int i = 0; i<amount; i++) {
+      draw.add(cards.remove(cards.size()-1));
+    }
+    System.out.println("Drew:" + draw);
+    System.out.println("Cards in deck:" + cards);
+    return draw;
+  }
+
+  public String player(String name) {
+    String id = "Default";
+    Date now = new Date();
+
+    id = name + now;
+    
+    if (Player0 == null) {
+      Player0 = id;
+    } else if (Player1 == null) {
+      Player1 = id;
+    } else {
+      return null;
+    }
+
+    return id;
   }
 }
--- back/GameServer.java
+++ back/GameServer.java
@@ -12,10 +12,11 @@ import java.util.ArrayList;
 
 public class GameServer {
     public static void main(String[] args) {
+        GameEngine engine = new GameEngine(); 
         try {
             HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
             // For now, we attach one simple handler to /join so we can test it
-            server.createContext("/draw", new Draw());
+            server.createContext("/draw", new Draw(engine));
 
             server.setExecutor(null);
             server.start();
@@ -26,6 +27,10 @@ public class GameServer {
     }
 
     static class Draw implements HttpHandler {
+        private GameEngine engine;
+        public Draw(GameEngine engine) {
+            this.engine = engine;
+        }
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             if (!"GET".equals(exchange.getRequestMethod())) {
@@ -33,10 +38,8 @@ public class GameServer {
                 sendJson(exchange, 405, msg);
                 return;
             }
-
-            GameEngine engine = new GameEngine(); 
             ArrayList<Integer> cards = new ArrayList<Integer>();
-            cards = engine.makeCards();
+            cards = engine.Draw(5);
 
             // For now, just echo back a fake playerId
             String response = "{\"status\": \"ok\", \"cards\": \"" + cards + "\"}";
@@ -44,6 +47,49 @@ public class GameServer {
             sendJson(exchange, 200, response);
         }
     }
+
+    static class start implements HttpHandler {
+        private GameEngine engine;
+        public Draw(GameEngine engine) {
+            this.engine = engine;
+        }
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            if (!"POST".equals(exchange.getRequestMethod())) {
+                String msg = "{\"error\": \"Only POST allowed\"}";
+                sendJson(exchange, 405, msg);
+                return;
+            }
+            
+
+            String body;
+            try (InputStream is = exchange.getRequestBody()) {
+                body = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+            }
+            System.out.println("Received: " + body);
+            String name;
+            try {
+                JSONObject json = new JSONObject(body);
+                name = json.getString("name");
+            } catch (JSONException e) {
+                sendJson(exchange, 400, "\"{error\": \"Not JSON\"}");
+                return;
+            }
+
+            ArrayList<Integer> cards = new ArrayList<Integer>();
+            engine.makeCards();
+            cards = engine.Draw(5);
+            String id = engine.player();
+
+            String response = "{\"status\": \"ok\", \"cards\": \"" + cards + "\", \"id\": \"" + id + "\"}";
+
+            sendJson(exchange, 200, response);
+        }
+    }
+
+
+
+
 
     static void sendJson(HttpExchange exchange, int statusCode, String json) throws IOException {
         byte[] bytes = json.getBytes(StandardCharsets.UTF_8);
